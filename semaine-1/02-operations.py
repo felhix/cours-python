8b6d324dbeb01b55dd878862c64e52931d397fa2
felhix <felix@hellojam.fr>
2017-05-15 16:15:47 +0200

    opérations

---
+++ semaine-1/02-operations.py
@@ -0,0 +1,25 @@
+#! python3
+# lucky.py - effectue quelques opérations de base et les affiche sur l'écran
+
+#addition
+print(2 + 3) #=> 5
+
+#soustraction
+print(4 - 2) #=> 2
+
+#multiplication
+print(2 * 3) #=> 6
+
+#division
+print(10 / 5) #=> 2.0
+
+#modulo
+print(13 % 2) #=> 1
+print(6 % 2) #=> 0 (car 6 = 2 * 3 + 0)
+
+#exponentiel
+print(2 ** 3) #=> 8
+
+#parenthèses
+print(2 + 3 * 6) # => 20
+print((2 + 3) * 6) # => 30
